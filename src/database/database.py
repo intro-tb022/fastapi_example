b99c641f93bb2898b2cc58a423bf97f73008fbe1
Fede Esteban <fede.est@gmail.com>
2025-04-02 20:32:40 -0300

    Style fixes

--- src/database/database.py
+++ src/database/database.py
@@ -1,6 +1,8 @@
+from fastapi import HTTPException, status
+
+
 from src.models import Alumno, AlumnoUpsert
 
-from fastapi import HTTPException, status
 
 class Database:
     def __init__(self):
@@ -8,17 +10,17 @@ class Database:
 
     def cargar_alumnos(self, alumnos: list[Alumno]):
         self.alumnos = alumnos
-    
+
     def list(self) -> list[Alumno]:
         return self.alumnos
-    
+
     def add(self, alumno_a_crear: AlumnoUpsert) -> Alumno:
         padron = len(self.alumnos) + 1
         alumno = Alumno(
             nombre=alumno_a_crear.nombre,
             apellido=alumno_a_crear.apellido,
             edad=alumno_a_crear.edad,
-            padron = padron
+            padron=padron,
         )
         self.alumnos.append(alumno)
         return alumno
@@ -27,13 +29,15 @@ class Database:
         for alumno in self.alumnos:
             if alumno.padron == padron:
                 return alumno
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Alumno not found")
-            
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Alumno not found"
+        )
+
     def delete(self, padron) -> Alumno:
         alumno = self.find(padron)
         self.alumnos.remove(alumno)
         return alumno
-    
+
     def update(self, padron, nuevo_alumno: AlumnoUpsert) -> Alumno:
         alumno = self.find(padron)
         alumno.nombre = nuevo_alumno.nombre
